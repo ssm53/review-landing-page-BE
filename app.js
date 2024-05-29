9c797358dd02eea987695a87f641562ff0122510
Shaun Menezes <shaunshanil95@gmail.com>
2024-05-30 03:34:53 +0800

    linking localhost to backend to test

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ import getAllReviewsRouter from "./src/controllers/getAllReviews.controllers.js"
 const app = express();
 app.use(morgan("combined"));
 app.use(cors()); // Use the cors middleware to allow cross-origin requests
+app.use(function (req, res, next) {
+  res.header("Access-Contorl-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.json()); // Add this middleware to parse JSON in request bodies
 app.use("/new-bad-review", newBadReviewRouter);
 app.use("/get-all-reviews", getAllReviewsRouter);
