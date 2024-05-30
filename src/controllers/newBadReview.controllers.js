c48e8392ace93bd9177c960232f2dff22a8d1143
Shaun Menezes <shaunshanil95@gmail.com>
2024-05-30 13:21:02 +0800

    added datetime to reviews post manny

--- src/controllers/newBadReview.controllers.js
+++ src/controllers/newBadReview.controllers.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { Prisma } from "@prisma/client";
 import prisma from "../utils/prisma.js";
+import { DateTime } from "luxon"; // Import luxon library
 
 const router = express.Router();
+
+const gmtTime = DateTime.local().setZone("Asia/Kuala_Lumpur");
+const malaysiaTime = gmtTime.plus({ hours: 8 });
 
 router.post("/", async (req, res) => {
   const data = req.body;
@@ -11,6 +15,7 @@ router.post("/", async (req, res) => {
     const newBadReview = await prisma.badReviews.create({
       data: {
         review: data.recommendation,
+        dateTime: malaysiaTime,
       },
     });
 
